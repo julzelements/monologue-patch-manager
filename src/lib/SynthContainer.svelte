776df19493ba33f89932d87b70f658809f7f8639
Julian Scharf <julzelements@gmail.com>
2025-11-25 20:18:25 +1000

    eg section

--- src/lib/SynthContainer.svelte
+++ src/lib/SynthContainer.svelte
@@ -9,6 +9,7 @@
   import OscillatorOne from "./sections/OscillatorOne.svelte";
   import OscillatorTwo from "./sections/OscillatorTwo.svelte";
   import Filter from "./sections/Filter.svelte";
+  import Envelope from "./sections/Envelope.svelte";
   import { CC_NAMES, prettyPanelSettings, type MonologueParameters, LABELS } from "@julzelements/monologue-midi";
 
   type PrettyPanelSettings = ReturnType<typeof prettyPanelSettings>;
@@ -92,37 +93,23 @@
     onValueChange={handleKnobChange}
   />
 
+  <!-- Envelope -->
+  <Envelope
+    typeValue={rawPatch?.panelSettings.envelope.type.value}
+    attackValue={rawPatch?.panelSettings.envelope.attack.value}
+    decayValue={rawPatch?.panelSettings.envelope.decay.value}
+    intensityValue={rawPatch?.panelSettings.envelope.intensity.value}
+    onValueChange={handleKnobChange}
+    onToggleChange={handleToggleChange}
+  />
+
   <!-- Knobs -->
-  <Knob
-    knobId={CC_NAMES.ampEgAttack}
-    name={"ATTACK"}
-    top={47}
-    left={541}
-    initialValue={rawPatch?.panelSettings.envelope.attack.value}
-    onValueChange={handleKnobChange}
-  />
-  <Knob
-    knobId={CC_NAMES.ampEgDecay}
-    name={"DECAY"}
-    top={47}
-    left={615.5}
-    initialValue={rawPatch?.panelSettings.envelope.decay.value}
-    onValueChange={handleKnobChange}
-  />
   <Knob
     knobId={CC_NAMES.lfoRate}
     name={"LFO RATE"}
     top={120}
     left={615.5}
     initialValue={rawPatch?.panelSettings.lfo.rate.value}
-    onValueChange={handleKnobChange}
-  />
-  <Knob
-    knobId={CC_NAMES.egInt}
-    name={"EG INT"}
-    top={47}
-    left={689}
-    initialValue={rawPatch?.panelSettings.envelope.intensity.value}
     onValueChange={handleKnobChange}
   />
   <Knob
@@ -142,24 +129,6 @@
     onValueChange={handleKnobChange}
   />
   <!-- Toggles -->
-  <VerticalToggle
-    id={CC_NAMES.egTarget}
-    name={"EG TARGET"}
-    top={39}
-    left={752.3}
-    positionNames={[...LABELS.EG_TARGET_LABELS]}
-    initialValue={rawPatch?.panelSettings.envelope.target.value ?? 0}
-    onValueChange={handleToggleChange}
-  />
-  <VerticalToggle
-    id={CC_NAMES.vco2Wave}
-    name={"EG TYPE"}
-    top={40}
-    left={497.5}
-    positionNames={[...LABELS.EG_TYPE_LABELS]}
-    initialValue={rawPatch?.panelSettings.envelope.type.value ?? 0}
-    onValueChange={handleToggleChange}
-  />
   <VerticalToggle
     id={CC_NAMES.lfoTarget}
     name={"LFO MODE"}
---
+++ src/lib/sections/Envelope.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Knob from "../knobs/Knob.svelte";
+  import VerticalToggle from "../toggles/VerticalToggle.svelte";
+  import { CC_NAMES, LABELS } from "@julzelements/monologue-midi";
+
+  export let typeValue: number | undefined = undefined;
+  export let attackValue: number | undefined = undefined;
+  export let decayValue: number | undefined = undefined;
+  export let intensityValue: number | undefined = undefined;
+  export let onValueChange: (name: string, value: number) => void;
+  export let onToggleChange: (name: string, value: string) => void;
+</script>
+
+<Knob knobId={CC_NAMES.ampEgAttack} name={"ATTACK"} top={47} left={541} initialValue={attackValue} {onValueChange} />
+<Knob knobId={CC_NAMES.ampEgDecay} name={"DECAY"} top={47} left={615.5} initialValue={decayValue} {onValueChange} />
+<Knob knobId={CC_NAMES.egInt} name={"EG INT"} top={47} left={689} initialValue={intensityValue} {onValueChange} />
+<VerticalToggle
+  id={CC_NAMES.vco2Wave}
+  name={"EG TYPE"}
+  top={40}
+  left={497.5}
+  positionNames={[...LABELS.EG_TYPE_LABELS].reverse()}
+  initialValue={typeValue ?? 0}
+  onValueChange={onToggleChange}
+/>
