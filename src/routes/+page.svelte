d0f0705bda462ec2938c9fa614dedd3d790b359c
Julian Scharf <julzelements@gmail.com>
2025-11-30 22:51:51 +1000

    We have a heading now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import SynthContainer from '$lib/SynthContainer.svelte';
+  import SynthContainer from "$lib/SynthContainer.svelte";
 </script>
+
+<h1>🎛️ Monologue Patch Manager 🎹</h1>
 
 <SynthContainer />
